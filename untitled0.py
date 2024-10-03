fd299a56f4d987f686d7da15c0f6ccf789bf196a
Lorenzo0627 <lsalazar1@unm.edu>
2024-10-03 15:18:32 -0600

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int height;
+    printf("Enter the height of the pyramid: ");
+    scanf("%d", &height);
+
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
